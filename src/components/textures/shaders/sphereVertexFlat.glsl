db85f92476cc88c9af3ec61b53564ef2eadc05df
Jeran <thejeran@gmail.com>
2025-10-07 15:21:07 +0200

    Forgot to implement the zero offset

--- src/components/textures/shaders/sphereVertexFlat.glsl
+++ src/components/textures/shaders/sphereVertexFlat.glsl
@@ -22,7 +22,7 @@ void main() {
     vec2 uv = giveUV(position);
     vec3 normal = normalize(position);
     float dispStrength = texture(map, uv).r;
-    vec3 newPos = position + (normal * dispStrength * displacement);
+    vec3 newPos = position + (normal * (dispStrength-displaceZero) * displacement);
     aPosition = position; //Pass out position for sphere frag
     vec4 worldPos = modelViewMatrix * vec4( newPos, 1.0 );
     gl_Position = projectionMatrix * worldPos;
