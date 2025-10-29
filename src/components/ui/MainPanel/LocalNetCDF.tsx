2d7e1ff00090fa964b401579ca6d90199bc4ecc2
Jeran <thejeran@gmail.com>
2025-10-29 16:41:48 +0100

    wasm try

--- src/components/ui/MainPanel/LocalNetCDF.tsx
+++ src/components/ui/MainPanel/LocalNetCDF.tsx
@@ -1,31 +1,38 @@
 "use client";
 import React, {ChangeEvent} from 'react'
 import { Input } from '../input'
-import { NetCDF4 } from '../../../../node_modules/netcdf4-wasm'
+import dynamic from 'next/dynamic';
 
-const LocalNetCDF = () => {
 
-  async function handleFileSelect(e: ChangeEvent<HTMLInputElement>){
-    const file = e.target.files?.[0]
-    if (file) { 
-      const path = file.webkitRelativePath
-      const nc = await new NetCDF4(path, "r");
-      console.log(nc)
+const LocalNetCDF = dynamic(async () => {
+  const { Dataset, setNetCDF4WasmPath } = await import('netcdf4-wasm');
 
+  // Define a React component that uses Dataset
+  function Component() {
+    async function handleFileSelect(e: ChangeEvent<HTMLInputElement>) {
+      const file = e.target.files?.[0];
+      if (file) {
+        const path = file.webkitRelativePath
+        const nc = Dataset(path, "r");
+        console.log(nc)
+      }
     }
 
-    console.log(file)
-  }
-
-  return (
-    <div>
-        <Input type="file" id="filepicker"
-          className='hover:drop-shadow-md hover:scale-[110%]'
-          style={{width:'200px', cursor:'pointer'}}
+    return (
+      <div>
+        <Input
+          type="file"
+          id="filepicker"
+          className="hover:drop-shadow-md hover:scale-[110%]"
+          style={{ width: '200px', cursor: 'pointer' }}
           onChange={handleFileSelect}
         />
-    </div>
-  )
-}
+      </div>
+    );
+  }
+  // Return the component type, not JSX
+  return Component;
+}, { ssr: false });
+
 
 export default LocalNetCDF
