d01bc810e4f796d0da03e6a3cab4df941b332b2c
Jeran <thejeran@gmail.com>
2025-11-07 16:35:03 +0100

    flat shader

--- src/components/textures/shaders/flatBlocksVert3D.glsl
+++ src/components/textures/shaders/flatBlocksVert3D.glsl
@@ -8,8 +8,6 @@ uniform vec3 textureDepths;
 uniform float aspect;
 uniform float displaceZero;
 uniform float displacement;
-uniform vec2 latBounds;
-uniform vec2 lonBounds;
 uniform float animateProg;
 
 #define PI 3.1415926535
