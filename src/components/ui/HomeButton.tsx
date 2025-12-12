26a71c4111683607940e2acc7dde0523479cf6ec
Lazaro Alonso <lazarus.alon@gmail.com>
2025-12-12 10:53:04 +0100

    update home button

--- src/components/ui/HomeButton.tsx
+++ src/components/ui/HomeButton.tsx
@@ -1,29 +1,32 @@
 "use client";
 
-import { HiHomeModern } from "react-icons/hi2";
+import Image from "next/image";
 import Link from "next/link";
+import logoHome from "public/logo-light.svg";
 import { Button } from "@/components/ui/button";
-import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
-import { usePathname } from "next/navigation";
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export default function HomeButton() {
-  const pathname = usePathname();
-  // Don't show Home button on home page
-  if (pathname === "/") {
-    return null;
-  }
-  return (
-    <Tooltip delayDuration={500}>
-      <TooltipTrigger asChild>
-        <Link href="/" aria-label="Home">
-          <Button variant="ghost" size="icon" className="cursor-pointer hover:scale-90 transition-transform duration-100 ease-out">
-            <HiHomeModern className="size-6"/>
-          </Button>
-        </Link>
-      </TooltipTrigger>
-      <TooltipContent side="bottom" align="start">
-        Home
-      </TooltipContent>
-    </Tooltip>
-  );
+	return (
+		<Tooltip delayDuration={500}>
+			<TooltipTrigger asChild>
+				<Link href="/" aria-label="Home">
+					<Button
+						variant="ghost"
+						size="icon"
+						className="cursor-pointer hover:scale-90 transition-transform duration-100 ease-out"
+					>
+						<Image src={logoHome} alt="logoMPI" height={48} />
+					</Button>
+				</Link>
+			</TooltipTrigger>
+			<TooltipContent side="bottom" align="start">
+				Home
+			</TooltipContent>
+		</Tooltip>
+	);
 }
