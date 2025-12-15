a919400bef8ca9c11db7fb1a0ef9850787c3b33a
Lazaro Alonso <lazarus.alon@gmail.com>
2025-12-15 17:08:05 +0100

    do npm and minimal nuxt config

---
+++ docs/nuxt.config.ts
@@ -0,0 +1,21 @@
+export default defineNuxtConfig({
+  extends: ['docus'],
+
+  compatibilityDate: '2025-07-18',
+
+  css: ['~/assets/css/main.css'],
+
+  site: {
+    name: 'browzarr.io'
+  },
+
+  // Disable server-only modules
+  mcp: false,
+
+  llms: false,
+
+  // Nitro static build config
+  nitro: {
+    preset: 'static',
+  }
+})
