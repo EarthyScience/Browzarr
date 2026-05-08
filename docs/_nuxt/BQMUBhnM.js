6e0bfd9e3f5a0d49b311e22703ca2ca67b498815
lazarusA <lazarus.alon@gmail.com>
2026-05-08 20:39:57 +0000

    Deploying to gh-pages from @ EarthyScience/Browzarr@6aeb90fe5893c193a0959c8ac76c155b0db09067 🚀

---
+++ docs/_nuxt/BQMUBhnM.js
@@ -0,0 +1,1 @@
+import{r as n}from"./LjxtB9-d.js";import a from"./cDIGsh8z.js";import{$ as s,a as t,p as e,s as i,o as p}from"./CkC6Bw6z.js";import"./BN_7HF1G.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,f)=>(p(),t(a,{color:"warning",icon:i(o).ui.icons.warning},{default:e(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
