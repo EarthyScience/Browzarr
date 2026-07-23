4cdbd06451cf459580e1b7d6406837d22827107c
Lazaro Alonso <lazarus.alon@gmail.com>
2026-07-23 20:50:42 +0200

    update to new function

--- src/components/computation/shaders/frag.glsl
+++ src/components/computation/shaders/frag.glsl
@@ -26,40 +26,10 @@ void main() {
         Color = vec4(0.0);
         return;
     }
-    if (threshold.y > threshold.x) {
-        if (d < threshold.x) {
-            if (useLowclip) { Color = lowclip; return; }
-            else { Color = vec4(0.0); return; }
-        }
-        if (d > threshold.y) {
-            if (useHighclip) { Color = highclip; return; }
-            else { Color = vec4(0.0); return; }
-        }
-    }
 
-    float normD = (threshold.y > threshold.x) ? clamp((d - threshold.x) / (threshold.y - threshold.x), 0.0, 1.0) : d;
-    if (colorScale == 1 && normD < logEps) {
-        if (useLowclip) Color = lowclip;
-        else Color = vec4(texture(cmap, vec2(0.0, 0.5)).rgb, 1.0);
-        return;
-    }
-    float scaledD = applyColorScale(normD, colorScale, logConstant, logEps, dataRange, minVal);
-    float rawSampLoc = scaledD * cScale + cOffset;
-
-    if (rawSampLoc < 0.0) {
-        if (useLowclip) Color = lowclip;
-        else Color = vec4(texture(cmap, vec2(0.0, 0.5)).rgb, 1.0);
-        return;
-    }
-    if (rawSampLoc > 1.0) {
-        if (useHighclip) Color = highclip;
-        else Color = vec4(texture(cmap, vec2(0.995, 0.5)).rgb, 1.0);
-        return;
-    }
-
-    float sampLoc = clamp(rawSampLoc, 0.0, 0.995);
-    vec4 color = texture(cmap, vec2(sampLoc, 0.5));
-    color.a = 1.0;
-
-    Color = color;
+    Color = evaluateColorScale(
+        d, threshold, 0.999, vec3(0.0), 0.0,
+        cmap, cScale, cOffset, colorScale, logConstant, logEps,
+        dataRange, minVal, lowclip, highclip, useLowclip, useHighclip
+    );
 }
